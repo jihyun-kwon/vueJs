775d49738dfa766e1cb10ad53891c4d56b10d934
jihyunKwon <jihyunkwon29@gmail.com>
2020-01-05 15:35:10 +0900

    [todo 앱 만들기] 기본 설정 및 test

---
+++ age-of-vuejs/vue-cli/src/a.vue
@@ -0,0 +1,2 @@
+npm install vue vue-loader vue-template-compiler webpack webpack-cli webpack-d
+ev-server babel-loader @babel/core @babel/preset-env css-loader vue-style-loader html-webpack-plugin 
---
+++ vue-todo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- 3. 컴포넌트 등록완료했다면 태그를 app태그 안에 추가 -->
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+<script>  //주의 : import와 export가 나뉘어져있으면 not defined 에러
+  //1. es6 방식으로 지역 components import
+  import TodoHeader from './components/TodoHeader.vue'
+  import TodoFooter from './components/TodoFooter.vue'
+  import TodoList from './components/TodoList.vue'
+  import TodoInput from './components/TodoInput.vue'
+
+  export default {  //2. 지역컴포넌트를 export하여 최상위 컴포넌트인 App.vue파일에 적용
+    components: {
+      'TodoHeader': TodoHeader,
+      'TodoFooter': TodoFooter,
+      'TodoList': TodoList,
+      'TodoInput': TodoInput,
+    }
+  }
+</script>
+
+<style>
+</style>
